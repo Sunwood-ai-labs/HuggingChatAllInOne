de4c40360bddd4ae1a718472c9fec5b8e2a7ade6
hAmAruKi <haru.maki.ch.0303@gmail.com>
2023-09-14 14:45:33 +0900

    update bat file

--- GetLatestBackEndSources.bat
+++ GetLatestBackEndSources.bat
@@ -1,12 +1,5 @@
 @echo off
 REM Its better to keep this, as we need to ensure the right commit is checked out.
-set DockerUserCommit=31e2253ae721ea80032283b9e85ffe51945e5a55
+
 set SourceFolder=Sources\HuggingChat\BackEnd
-
-REM rmdir /s /q %SourceFolder%
-
 git clone https://github.com/huggingface/text-generation-inference %SourceFolder%
-
-cd %SourceFolder%
-git checkout %DockerUserCommit%
-cd ..
--- GetLatestFrontEnd.bat
+++ GetLatestFrontEnd.bat
@@ -1,12 +1,6 @@
 @echo off
 REM Its better to keep this, this repo has to be maintained properly and I cannot just assume their latest changes will not impact the docker image
-set DockerUserCommit=0aa57debc7f7ffecccb8dc16b2f4b35816fbd817
+
 set SourceFolder=Sources\HuggingChat\FrontEnd
-
-REM rmdir /s /q %SourceFolder%
-
 git clone https://github.com/bodaay/chat-ui %SourceFolder%
 
-cd %SourceFolder%
-git checkout %DockerUserCommit%
-cd ..
